f2e74d817ce57ce01f9b2450347837ea4cfae87e
Chilsem <tamsamani.ibrahim115@gmail.com>
2025-05-18 14:25:39 +0100

    Update ft_putnbr_fd.c

--- libft_functions/ft_putnbr_fd.c
+++ libft_functions/ft_putnbr_fd.c
@@ -12,24 +12,62 @@
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+static int	fill_digits(int n, char *temp)
 {
+	int	i;
+
+	i = 0;
+	while (n > 0)
+	{
+		temp[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (i);
+}
+
+static void	reverse_and_copy(char *buffer, char *temp, int len, int is_negative)
+{
+	int	i;
+
+	i = 0;
+	if (is_negative)
+		buffer[i++] = '-';
+	while (len > 0)
+		buffer[i++] = temp[--len];
+	buffer[i] = '\0';
+}
+
+static void	ft_itoa_stack(int n, char *buffer)
+{
+	char	temp[12];
+	int		len;
+	int		is_negative;
+
+	if (n == 0)
+	{
+		buffer[0] = '0';
+		buffer[1] = '\0';
+		return ;
+	}
 	if (n == -2147483648)
 	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		n = 147483648;
+		ft_strcpy(buffer, "-2147483648");
+		return ;
 	}
+	is_negative = 0;
 	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
+		is_negative = 1;
 		n = -n;
 	}
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd((n % 10) + '0', fd);
-	}
+	len = fill_digits(n, temp);
+	reverse_and_copy(buffer, temp, len, is_negative);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	buffer[12];
+
+	ft_itoa_stack(n, buffer);
+	write(fd, buffer, ft_strlen(buffer));
 }
